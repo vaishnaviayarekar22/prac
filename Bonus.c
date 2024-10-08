c1bb19fb50c22a2cc1739e21ae142042b31a68ab
vaishnaviayarekar22 <vaishnaviayarekar22@gmail.com>
2024-10-08 18:06:04 +0530

    Update Bonus.c

--- Bonus.c
+++ Bonus.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int joining_year,bonus,current_year;
+    int joining_year,bonus,current_year,working_year;
     printf("Enter The joining_year=");
     scanf("%d",&joining_year);
 
@@ -10,9 +10,9 @@ int main()
     printf("Enter current_year=");
     scanf("%d",&current_year);
 
-    joining_year=current_year-joining_year;
+    working_year=current_year-joining_year;
 
-    if(joining_year>=3)
+    if(workining_year>=3)
     {
         printf("The Bonus is 2500",bonus);
     }
