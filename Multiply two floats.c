64488920b0bba7ba6c56ed16ff17db211209a98a
vaishnaviayarekar22 <vaishnaviayarekar22@gmail.com>
2024-11-11 22:23:16 +0530

    Add files via upload

---
+++ Multiply two floats.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    float num1,num2,mul;
+    printf("Enter The Number=");
+    scanf("%f",&num1);
+
+    printf("Enter The Number=");
+    scanf("%f",&num2);
+
+    mul=num1*num2;
+
+    printf(" %f",mul);
+}
