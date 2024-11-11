3ec29e4fd298f7720259d26a5b5fde0450fadb4e
vaishnaviayarekar22 <vaishnaviayarekar22@gmail.com>
2024-11-11 20:33:35 +0530

    Add files via upload

---
+++ Add two integers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,add;
+    printf("Enter the num1=");
+    scanf("%d",&num1);
+    printf("Enter the num2=");
+    scanf("%d",&num2);
+    add=num1+num2;
+    printf("The addition of %d and %d is %d",num1,num2,add);
+}
